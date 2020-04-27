975b7c1bdaa5379121e8e5e88efa917742c6b454
abrych <arkadiusz.brych@external.t-mobile.pl>
2020-04-27 20:20:01 +0200

    second part of first lesson

--- app/src/main/java/com/example/mycardapp/MainActivity.kt
+++ app/src/main/java/com/example/mycardapp/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mycardapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_linear)
+        setContentView(R.layout.activity_main_constraint)
+
+        findViewById<Button>(R.id.petDogButton).setOnClickListener {
+            //Log.d("MainActivity", "button clicked")
+            Toast.makeText(this, "on button clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 }
